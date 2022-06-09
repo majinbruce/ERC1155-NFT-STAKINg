a1d2014a163c91c2cfd92572bc0bc6a33d1ade66
omkar <omkar.c.344@gmail.com>
2022-06-09 14:13:19 +0530

    added test cases

--- tests/stakingTest.js
+++ tests/stakingTest.js
@@ -5,11 +5,10 @@ describe("Staking", () => {
   let owner;
   let addr1;
   let addr2;
-  let addr3;
 
   let token;
   let TOKEN;
-  let totalSupply = ethers.utils.parseUnits("1", 17);
+  let totalSupply = 100000000 * ethers.utils.parseUnits("1", 18);
   let NFT;
   let nft;
   let STAKING;
@@ -42,13 +41,16 @@ describe("Staking", () => {
     await nft.connect(addr2).setApprovalForAll(staking.address, true);
 
     // transfer 1000 silver nfts to addr1,addr2
-    nft.safeTransferFrom(nft.address, addr1, 0, 1000, "");
-    nft.safeTransferFrom(nft.address, addr2, 0, 1000, "");
+    await nft.safeTransferFrom(owner.address, addr1.address, 0, 1000, "0x00");
+    await nft.safeTransferFrom(owner.address, addr2.address, 0, 1000, "0x00");
+
+    // stake 1000 nft tokens from addr 1
+    await staking.connect(addr1).stakeNFT(0, 1000);
   });
 
   describe("minting nfts", function () {
     it("should assign the right amount of nfts to the owner of the contract", async function () {
-      expect(await nft.balanceOf(owner.address, 0)).to.equal(10 ** 4);
+      expect(await nft.balanceOf(owner.address, 0)).to.equal(8000);
       expect(await nft.balanceOf(owner.address, 1)).to.equal(10 ** 3);
 
       expect(await nft.balanceOf(owner.address, 2)).to.equal(1);
@@ -57,7 +59,41 @@ describe("Staking", () => {
 
   describe("staking", function () {
     it("should let users stake nfts", async () => {
-      await staking.stakeNFT(0, 1000);
+      // transfer 1000 silver nfts to addr1,addr2
+      const _tokenID = 0;
+      const _amount = 1000;
+
+      const stakedNft = await staking.stakedNFTs(addr1.address, _tokenID);
+
+      // check if the amount was rightly set in mapping
+      expect(await stakedNft.amount).to.equal(_amount);
+
+      //check if transfer of nft was done correctly
+      expect(await nft.balanceOf(staking.address, _tokenID)).to.equal(_amount);
+      expect(await nft.balanceOf(addr1.address, _tokenID)).to.equal(0);
+    });
+  });
+
+  describe("unstaking", function () {
+    it("should let users unstakestake nfts & receive reward tokens", async () => {
+      const _tokenID = 0;
+      const _amount = 1000;
+      const oneMonthInSeconds = 2629743;
+      const balanceBeforeStaking = await token.balanceOf(addr1.address);
+      console.log("\n", balanceBeforeStaking, "balanceBeforeStaking");
+      // Increase evm time by 2 months.
+      await ethers.provider.send("evm_increaseTime", [oneMonthInSeconds * 2]);
+
+      await staking.connect(addr1).unStakeNFT(_tokenID, _amount);
+
+      expect(await token.balanceOf(addr1.address)).to.not.equal(
+        balanceBeforeStaking
+      );
+      console.log(
+        "\n",
+        await token.balanceOf(addr1.address),
+        "balance after staking"
+      );
     });
   });
 });
